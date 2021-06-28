08f69f066b4851a5da74e870315b19b0c8cc8188
tylan460 <74316240+tylan460@users.noreply.github.com>
2021-06-28 14:09:15 +0200

    Update Task 0.8.js
    Fixed all minor issues

--- Task 0.8.js
+++ Task 0.8.js
@@ -2,10 +2,14 @@ function convertNum(num) {
   var hour = num / 60;
   hour = Math.floor(hour);
   var minutes = num % 60;
-  if (hour == 1) {
-    return hour + " " + "hour, " + " "  + minutes + " " + "minutes"
+   if (minutes == 1) {
+    return hour + " " + "hours, " + " " + minutes + " " + "minute";
+ } else if (num < 60) {
+   return hour + " " + "hours, " + " " + minutes + " " + "minutes";
+ } else if (hour == 1) {
+    return hour + " " + "hour, " + " "  + minutes + " " + "minutes";
   } else if (hour > 1) {
-    return hour + " " + "hours, " + " "  + minutes + " " + "minutes"
-  }
+    return hour + " " + "hours, " + " "  + minutes + " " + "minutes";
 }
-console.log(convertNum(71))
+}
+console.log(convertNum(121))
