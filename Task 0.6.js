c67359694e85a24201fa6aa27c7191af360db635
tylan460 <74316240+tylan460@users.noreply.github.com>
2021-06-28 14:05:58 +0200

    Update Task 0.6.js
    Removed commented task ,used '...arguments' as parameters

--- Task 0.6.js
+++ Task 0.6.js
@@ -1,13 +1,5 @@
-function maxnum(num1, num2, num3) {
-/*  if (num1 > num2 && num1 > num3) {
-  return num1;
-} else if (num2 > num1 && num2 > num3) {
-  return num2;
-} else  {
-  return num3;
-}  initial task instructions completed*/
+function maxnum(...arguments) {
 
- 
  var newMax = arguments[0];
  for (var i = 0 ; i < arguments.length; i++ ) {
    if (arguments[i] > newMax) {
